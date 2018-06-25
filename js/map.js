9d756e875df3cbfac0fd0e488161e6a32d666609
EkaterinaEfimova <abramiankate@yandex.ru>
2018-06-26 01:01:28 +0300

    Добавила возможность перетаскивать главную метку

--- js/map.js
+++ js/map.js
@@ -309,10 +309,10 @@ var activePage = function () {
   activeMap();
   offers = createOfferInfo();
   fillMap();
-  inputAdress(mainPinStartX, mainPinStartActiveY);
+  //inputAdress(mainPinStartX, mainPinStartActiveY);
 };
 
-mainPin.addEventListener('mouseup', activePage);
+//mainPin.addEventListener('mouseup', activePage);
 
 // Подстановка координат в поле адрес
 var inputAdress = function (x, y) {
@@ -446,3 +446,74 @@ var adFormSubmitClickHandler = function () {
 };
 
 submitForm.addEventListener('click', adFormSubmitClickHandler);
+
+// Модуль module5-task1
+
+var COORD_Y_MIN = 130;
+var COORD_Y_MAX = 630;
+
+var coordXMin = 0;
+var coordXMax = map.offsetWidth - mainPin.offsetWidth;
+var coordYMin = COORD_Y_MIN - mainPin.offsetHeight - PIN_ARROW_HEIGTH;
+var coordYMax = COORD_Y_MAX - mainPin.offsetHeight - PIN_ARROW_HEIGTH;
+
+mainPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var mouseMoveHandler = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    var coordPinX = mainPin.offsetLeft - shift.x;
+    var coordPinY = mainPin.offsetTop - shift.y;
+
+    if (coordPinX > coordXMin && coordPinX < coordXMax) {
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+    }
+
+    if (coordPinY > coordYMin && coordPinY < coordYMax) {
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+    }
+
+    var mainPinCoordX = Math.round(coordPinX + mainPin.offsetWidth / 2);
+    var mainPinCoordY = Math.round(coordPinY + mainPin.offsetHeight + PIN_ARROW_HEIGTH);
+    
+    inputAdress(mainPinCoordX, mainPinCoordY);
+  };
+
+  var mouseUpHandler = function (upEvt) {
+    upEvt.preventDefault();
+
+    if (!checkMapActive()) {
+      activePage();
+    }
+
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+
+  };
+
+  document.addEventListener('mousemove', mouseMoveHandler);
+  document.addEventListener('mouseup', mouseUpHandler);
+});
+
+
+// Функция для проверки активности карты
+
+var checkMapActive = function () {
+  return !map.classList.contains('map--faded');
+};
